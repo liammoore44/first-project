2661ab04ea8786d4fc79081bb07b87080110e6f6
liammoore44 <52430585+liammoore44@users.noreply.github.com>
2019-08-07 19:05:59 +0100

    Add files via upload

--- uni.py
+++ uni.py
@@ -1,12 +1,31 @@
 import datetime
 from datetime import timedelta
 
-#for first draft assume class begins at 9am every day
 
-class_time = timedelta(hours = 9 )
 
-amount_of_sleep = timedelta(hours = 7, minutes = 30)
+class Day:
+    def __init__(self, day_name, class_time):
+        self.day_name = day_name
+        self.class_time = timedelta(hours = float(class_time))
 
-bed_time = class_time - amount_of_sleep
+    def bed_time(self):
+        hrs = input(f"""
+You have class on {self.day_name} at {self.class_time}, how many hours of sleep would you like?
+        """)
+        hrs_sleep = timedelta(hours = float(hrs))
+        leway = timedelta(minutes = 15)
+        bed_time = self.class_time - hrs_sleep - leway
+        print(f"You should be in bed by: {bed_time}")
 
-print(bed_time)
+
+monday = Day("Monday", 9.5)
+tuesday = Day("Tuesday", 6)
+wednesday = Day("Wednesday", 7)
+thursday = Day("Thursday", 8)
+friday = Day("Friday", 6.5)
+
+monday.bed_time()
+tuesday.bed_time()
+wednesday.bed_time()
+thursday.bed_time()
+friday.bed_time()
